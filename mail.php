e09cd5fde5671e42e0c847253a843a422263cdf4
Vladislav Chebotarev <wczebotarew@gmail.com>
2016-11-14 22:11:09 +0100

    first commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+$recepient = "massage@gmail.com";
+$sitename = "MASSAGE.kz";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone ";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
